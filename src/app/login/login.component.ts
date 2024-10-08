d3386ee15be1832e4a3541d07db80538c5a6827a
Mateus Robers <robersmateus@gmail.com>
2024-10-08 12:35:42 -0300

    terminando a página de login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  submith() {
+    const email = (document.getElementById("email") as HTMLInputElement)
+    const pass = (document.getElementById("pass") as HTMLInputElement)
+
+    const body = {
+      email: email.value,
+      password: pass.value
+    }
+
+    fetch("http://localhost:8080/user", { method: "POST", body: JSON.stringify(body) })
+      .then(resposta => { console.log("deu bom") })
+      .catch(e => console.error("deu erro"))
+  }
+
 }
